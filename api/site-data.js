92f811d31d33c64f63ff0132d5a62248dd2fda80
Jaweriya Saroash <jaweriyasofficial@gmail.com>
2026-09-06 01:15:51 +0500

    Delete api directory

--- api/site-data.js
+++
@@ -1,9 +0,0 @@
-const { getJsonFile, dataPath } = require('./_github');
-
-module.exports = async function handler(req,res){
-  if(req.method !== 'GET') return res.status(405).json({error:'Method not allowed.'});
-  try{
-    const result = await getJsonFile(dataPath('data'));
-    return res.status(200).json({data: result.data || null});
-  }catch(err){ return res.status(500).json({error:err.message || 'Unable to load site data.'}); }
-};
